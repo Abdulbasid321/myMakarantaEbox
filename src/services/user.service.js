db5f343e52b32e308ec485cc118bdc75b2a08aff
abdulbasid <abdulbasidhussain9@gmail.com>
2025-07-17 13:37:40 +0100

    update

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -20,47 +20,47 @@ const getUserById = async (userId) => {
 };
 
 // const updateUser = async (userId, updateData) => {
-    const user = await UserRepository.getUserById(userId);
-    if (!user) {
-        throw { isSuccess: false, message: 'User not found', user: null };
-    }
+    // const user = await UserRepository.getUserById(userId);
+    // if (!user) {
+    //     throw { isSuccess: false, message: 'User not found', user: null };
+    // }
 
-    if (updateData.email) {
-        const existingUser = await UserRepository.getUser({ email: updateData.email });
-        if (existingUser && existingUser._id.toString() !== userId) {
-            throw { isSuccess: false, message: 'This email is already registered.', user: null };
-        }
-    }
+    // if (updateData.email) {
+    //     const existingUser = await UserRepository.getUser({ email: updateData.email });
+    //     if (existingUser && existingUser._id.toString() !== userId) {
+    //         throw { isSuccess: false, message: 'This email is already registered.', user: null };
+    //     }
+    // }
     
-    if (updateData.phone) {
-        const existingUser = await UserRepository.getUser({ phone: updateData.phone });
-        if (existingUser && existingUser._id.toString() !== userId) {
-            throw { isSuccess: false, message: 'This phone number is already registered.', user: null };
-        }
-    }
+    // if (updateData.phone) {
+    //     const existingUser = await UserRepository.getUser({ phone: updateData.phone });
+    //     if (existingUser && existingUser._id.toString() !== userId) {
+    //         throw { isSuccess: false, message: 'This phone number is already registered.', user: null };
+    //     }
+    // }
 
-    if (updateData.userName) {
-        const existingUser = await UserRepository.getUser({ userName: updateData.userName });
-        if (existingUser && existingUser._id.toString() !== userId) {
-            throw { isSuccess: false, message: 'This username is taken.', user: null };
-        }
-    }
+    // if (updateData.userName) {
+    //     const existingUser = await UserRepository.getUser({ userName: updateData.userName });
+    //     if (existingUser && existingUser._id.toString() !== userId) {
+    //         throw { isSuccess: false, message: 'This username is taken.', user: null };
+    //     }
+    // }
 
-    if (updateData.password) {
-        updateData.password = await hashPassword(updateData.password);
-    }
+    // if (updateData.password) {
+    //     updateData.password = await hashPassword(updateData.password);
+    // }
 
-    updateData.updatedAt = new Date();
+    // updateData.updatedAt = new Date();
 
-    const newUser = await UserRepository.updateUserById(userId, updateData, { new: true, runValidators: true });
+    // const newUser = await UserRepository.updateUserById(userId, updateData, { new: true, runValidators: true });
 
-    if (!newUser) {
-        throw { isSuccess: false, message: 'User update failed', user: null };
-    }
+    // if (!newUser) {
+    //     throw { isSuccess: false, message: 'User update failed', user: null };
+    // }
 
-    newUser.password = undefined;
+    // newUser.password = undefined;
 
-    return { isSuccess: true, message: 'User updated successfully', user: newUser };
+    // return { isSuccess: true, message: 'User updated successfully', user: newUser };
 // };
 
 
